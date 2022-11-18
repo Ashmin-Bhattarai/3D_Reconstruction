3f8a932b5ab44847320c7578e23198d79115cb85
Ashmin-Bhattarai <koushal.ashmin@gmail.com>
2022-11-18 23:33:15 +0545

    test commit

--- source/main.py
+++ source/main.py
@@ -5,7 +5,7 @@ import sfm
 import os
 import pathlib
 
-dataset_folder_name = 'fountainN'
+dataset_folder_name = 'fountain'
 
 root_path = pathlib.Path(__file__).parent.parent
 dataset_path = os.path.join(root_path, 'datasets' ,dataset_folder_name)
