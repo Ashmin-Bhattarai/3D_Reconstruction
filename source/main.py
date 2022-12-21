5a4ffe656411d460ac6d8c78c0762895aea057e8
Devraj Neupane <asminneupane79@gmail.com>
2022-12-21 20:00:59 +0545

    :ambulance: temporary fix for sorted views

--- source/main.py
+++ source/main.py
@@ -7,6 +7,7 @@ import sfm
 import view
 from utils import sort_views
 
+# import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 
@@ -20,24 +21,17 @@ def main() -> None:
     views = view.create_views(dataset_path)
     matches = match.create_matches(views)
     sorted_views = sort_views(matches)
+    matches = match.create_matches(sorted_views)  # Temporary Fix
 
-
-    # print(sorted_views)
     # print([image.name for image in sorted_views])
     # pprint(next(iter(matches.items()))[1].number_of_inliers())
     # pprint(next(iter(matches.items()))[1].view1.__dict__)
     # pprint(next(iter(matches.items()))[1].view2.__dict__)
     # pprint(matches.items())
     # pprint(iter(matches).__dir__)
-    
 
-    sfm_obj = sfm.SFM(sorted_views, matches)  # <<---- !!!(error here)!!!
-    # works with (views, matches) but not with (sorted_views, matches)
-    
-    
+    sfm_obj = sfm.SFM(sorted_views, matches)
     sfm_obj.reconstruct()
-
-    
 
     print("\n*****************Done matching*****************")
 
